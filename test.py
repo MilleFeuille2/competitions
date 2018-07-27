b1188341c9873dd6d75c10d4894e83203c220f4f
Hirotake Yasuda <ysdhrtk.227@gmail.com>
2018-07-27 21:38:57 +0900

    no message

--- test.py
+++ test.py
@@ -6,13 +6,26 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 df = pd.read_csv('../data/application_train.csv')
-X = df.iloc[:, :-1]
-y = df.iloc[:, -1]
+X = df.iloc[:, 2:]
+y = df['TARGET']
+
+X = X[['CNT_CHILDREN', 'AMT_INCOME_TOTAL']]
+
+# for column in df.columns:
+#     print(column)
+#     print(df.loc[0, column])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+print(X_train.index[:3])
+print(y_train.index[:3])
 
 clf = RandomForestClassifier(random_state=0)
-clf.fit(X, y)
+clf.fit(X_train, y_train)
 
-print(df.head())
+score_train = clf.score(X_train, y_train)
+score_test = clf.score(X_test, y_test)
 
+print(score_train, score_test)
 
 
